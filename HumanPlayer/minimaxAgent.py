2cef7c7b0c7e4e7b4d504f6a9d97b1fc384b44b8
boynextdoor <chize@shanghaitech.edu.cn>
2022-12-18 21:48:30 +0800

    Start implementing minimaxAgent

---
+++ HumanPlayer/minimaxAgent.py
@@ -0,0 +1,54 @@
+from abc import ABC
+from typing import Optional
+
+from agent import Agent
+from utils import Piece, Player
+import math, copy
+
+
+class minimaxAgent(Agent):
+
+    def __init__(self, direction: Player, depth: int):
+        super().__init__(direction)
+        self.index = 0
+        self.depth = depth
+        self.playerSide = direction
+
+    def getLegalMoves(self, agentIndex) -> list[tuple[tuple[int, int], tuple[int, int]]]:
+
+        allPieces = self.game.getSide(self.playerSide)
+        legalMoves = []
+        for piecePosition in allPieces:
+            for targetPosition in self.game.getRange(piecePosition):
+                if self.game.isValidMove(piecePosition, targetPosition):
+                    legalMoves.append((piecePosition, targetPosition))
+        return legalMoves
+
+    def evaluationFunction(self):
+        return 0
+
+    def step(self) -> tuple[tuple[int, int], tuple[int, int]]:
+
+        def maxValue(state, depth, agentIndex, alpha, beta):
+            if depth == self.depth * 2:
+                return self.evaluationFunction()
+            value = -math.inf
+            legalMoves = self.getLegalMoves(agentIndex)
+            for action in legalMoves:
+                value = max(value, minValue(depth + 1, agentIndex ^ 1, alpha, beta))
+                if value > beta:
+                    return value
+                alpha = max(alpha, value)
+            return value
+
+        def minValue(state, depth, agentIndex, alpha, beta):
+            if depth == self.depth * 2:
+                return self.evaluationFunction()
+            value = math.inf
+            legalMoves = self.getLegalMoves()
+            for action in legalMoves:
+                value = min(value, maxValue(depth + 1, agentIndex ^ 1, alpha, beta))
+                if value < alpha:
+                    return value
+                beta = min(beta, value)
+            return value
