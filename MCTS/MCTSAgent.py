fa2ecfe82827e15394abfdf79842aa2abdc31cc3
NickQin <nick0418@163.com>
2022-12-23 17:47:26 +0800

    REPUSH

--- MCTS/MCTSAgent.py
+++ MCTS/MCTSAgent.py
@@ -156,13 +156,13 @@ class MCTSAgent(Agent):
     @staticmethod
     def backup(node: MCTSnode, reward: float, direction: Player):
         # TODO: Verify direction/Player. Verify the Pieces got by indexing with actions.
-        evaluate = [[0, 0, 0, 0, 0, 0, 0],
+        evaluate = [[0, 0, 0, 1, 1, 1, 1],
                     [0, 0, 0, 9.5, 14.5, 10, 7],
                     [0, 0, 0, 9.5, 14.5, 10, 7],
-                    [0, 5.5, 5.5, 7.5, 12.5, 8, 5],
-                    [0, 0.5, 0.5, 2.5, 7.5, 3, 0],
-                    [0, 5, 5, 7, 12, 7.5, 4.5],
-                    [0, 8, 8, 10, 15, 10.5, 7.5]]
+                    [50, 5.5, 5.5, 7.5, 12.5, 8, 5],
+                    [50, 0.5, 0.5, 2.5, 7.5, 3, 0],
+                    [50, 5, 5, 7, 12, 7.5, 4.5],
+                    [50, 8, 8, 10, 15, 10.5, 7.5]]
         whole = np.zeros((15, 15))
         whole[1:8, 8:15] = np.array(evaluate)
         whole[8:15, 1:8] = np.array(evaluate)
@@ -173,10 +173,10 @@ class MCTSAgent(Agent):
             consumer = int(parent.state.board[action[0][0]][action[0][1]])
             consumee = int(parent.state.board[action[1][0]][action[1][1]])
             if node.state.myself == direction:
-                reward -= whole[consumer][consumee]
+                reward = 0.9 * reward - whole[consumer][consumee]
                 node.quality_value += reward
             else:
-                reward += whole[consumer][consumee]
+                reward = 0.9 * reward + whole[consumer][consumee]
                 node.quality_value -= reward
             node = node.parent
         node.visit_time += 1
