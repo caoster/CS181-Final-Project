6bc66944857c1d8dff0ba864e92790b5174761b6
caoster <caoster@icloud.com>
2023-01-01 15:40:15 +0800

    Fix more warnings.

--- ReinforcementLearning/qlearningAgent.cpp
+++ ReinforcementLearning/qlearningAgent.cpp
@@ -2,8 +2,7 @@
 #include <algorithm>
 #include <random>
 
-Action QlearningAgent::step()
-{
+Action QlearningAgent::step() {
     Board board = game->getGameState().board;
     Action action = getAction(board);
     last_action = action;
@@ -12,21 +11,18 @@ Action QlearningAgent::step()
     GameState nextstate = last_state.getNextState(last_action);
     nextstate.swapDirection();
     Player winner = nextstate.getWinner();
-    if (winner == direction)
-    {
+    if (winner == direction) {
         float old_estimate = q_value[{board, last_action}];
         q_value.set({board, last_action}, (1 - myalpha) * old_estimate + myalpha * myreward);
     }
     return action;
 }
 
-Counter QlearningAgent::getQValueBoard()
-{
+Counter QlearningAgent::getQValueBoard() {
     return q_value;
 }
 
-void QlearningAgent::update(Action action)
-{
+void QlearningAgent::update(Action action) {
     Board lastboard = last_state.board;
     float old_estimate = getQValue(lastboard, last_action);
     Board board = game->getGameState().board;
@@ -39,27 +35,22 @@ void QlearningAgent::update(Action action)
     q_value.set({lastboard, last_action}, (1 - myalpha) * old_estimate + myalpha * sample);
 }
 
-float QlearningAgent::getQValue(const Board &current_board, Action action)
-{
+float QlearningAgent::getQValue(const Board &current_board, Action action) {
     return q_value[{current_board, action}];
 }
 
-Action QlearningAgent::computeActionFromQValues(Board current_board)
-{
+Action QlearningAgent::computeActionFromQValues(const Board &current_board) {
     std::vector<float> q_list;
     auto all_actions = game->getGameState().getLegalActionsBySide(direction);
-    for (auto &action : all_actions)
-    {
+    for (auto &action: all_actions) {
         float qvalue = getQValue(current_board, action);
         q_list.push_back(qvalue);
     }
     float max_q = *max_element(q_list.begin(), q_list.end());
-    std::vector<unsigned int> max_index;
+    std::vector<size_t> max_index;
 
-    for (unsigned int i = 0; i < q_list.size(); i++)
-    {
-        if (q_list[i] == max_q)
-        {
+    for (size_t i = 0; i < q_list.size(); i++) {
+        if (q_list[i] == max_q) {
             max_index.push_back(i);
         }
     }
@@ -67,137 +58,84 @@ Action QlearningAgent::computeActionFromQValues(Board current_board)
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(0, len);
-    unsigned int index = dist(rng);
+    size_t index = dist(rng);
     return all_actions[index];
 }
 
-float QlearningAgent::getReward(GameState state, Action action, Player player)
-{
+float QlearningAgent::getReward(GameState state, Action action, Player player) {
     float score = 0;
     GameState nextstate = last_state.getNextState(last_action);
     nextstate.swapDirection();
     Player winner = nextstate.getWinner();
-    if (winner == player)
-    {
+    if (winner == player) {
         score = 1000000;
-    }
-    else if (winner == player.reverse())
-    {
+    } else if (winner == player.reverse()) {
         score = -1000000;
-    }
-    else
-    {
+    } else {
         std::vector<Position> myPiece = state.getSide(player);
-        for (Position &piece : myPiece)
-        {
+        for (Position &piece: myPiece) {
             auto x = piece.first;
             auto y = piece.second;
             Piece pieceType = state[piece];
-            score += (pieceValue[pieceType.value()] * pieceScore[pieceType.value()][x][y]);
+            score += static_cast<float>(pieceValue[pieceType.value()] * pieceScore[pieceType.value()][x][y]);
         }
         Piece eatenPieceType = state[action.second];
-        switch (eatenPieceType.value())
-        {
-        case Piece::NoneType:
-        {
-            break;
-        }
-        case Piece::BSoldier:
-        {
-            score += 1;
-            break;
-        }
-        case Piece::RSoldier:
-        {
-            score += 1;
-            break;
-        }
-        case Piece::BAdvisor:
-        {
-            score += 2;
-            break;
-        }
-        case Piece::RAdvisor:
-        {
-            score += 2;
-            break;
-        }
-        case Piece::BElephant:
-        {
-            score += 2;
-            break;
-        }
-        case Piece::RElephant:
-        {
-            score += 2;
-            break;
-        }
-        case Piece::BHorse:
-        {
-            score += 4;
-            break;
-        }
-        case Piece::RHorse:
-        {
-            score += 4;
-            break;
-        }
-        case Piece::BCannon:
-        {
-            score += float(4.5);
-            break;
-        }
-        case Piece::RCannon:
-        {
-            score += float(4.5);
-            break;
-        }
-        case Piece::BChariot:
-        {
-            score += 9;
-            break;
-        }
-        case Piece::RChariot:
-        {
-            score += 9;
-            break;
-        }
-        case Piece::BGeneral:
-        {
-            break;
-        }
-        case Piece::RGeneral:
-        {
-            break;
-        }
+        switch (eatenPieceType.value()) {
+            case Piece::BSoldier:
+            case Piece::RSoldier: {
+                score += 1;
+                break;
+            }
+            case Piece::BAdvisor:
+            case Piece::RAdvisor:
+            case Piece::BElephant:
+            case Piece::RElephant: {
+                score += 2;
+                break;
+            }
+            case Piece::BHorse:
+            case Piece::RHorse: {
+                score += 4;
+                break;
+            }
+            case Piece::BCannon:
+            case Piece::RCannon: {
+                score += 4.5f;
+                break;
+            }
+            case Piece::BChariot:
+            case Piece::RChariot: {
+                score += 9;
+                break;
+            }
+            case Piece::NoneType:
+            case Piece::BGeneral:
+            case Piece::RGeneral: {
+                break;
+            }
         }
     }
     return score;
 }
 
-bool QlearningAgent::flipcoin()
-{
+bool QlearningAgent::flipcoin() const {
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(0, 100);
-    float r = dist(rng) / float(100.0);
+    float r = dist(rng) / 100.0f;
     return r < myepsilon;
 }
 
-Action QlearningAgent::getAction(Board board)
-{
+Action QlearningAgent::getAction(const Board& board) {
     std::vector<Action> all_actions = game->getGameState().getLegalActionsBySide(direction);
     Action action;
-    if (flipcoin())
-    {
+    if (flipcoin()) {
         size_t len = all_actions.size();
         std::random_device dev;
         std::mt19937 rng(dev());
         std::uniform_int_distribution<std::mt19937::result_type> dist(0, len);
         action = all_actions[dist(rng)];
-    }
-    else
-    {
+    } else {
         action = computeActionFromQValues(board);
     }
     return action;
--- ReinforcementLearning/qlearningAgent.h
+++ ReinforcementLearning/qlearningAgent.h
@@ -1,5 +1,5 @@
-#ifndef CS181_FINAL_PROJECT_CPP_qlearningAgent_H
-#define CS181_FINAL_PROJECT_CPP_qlearningAgent_H
+#ifndef CS181_FINAL_PROJECT_CPP_QLEARNING_AGENT_H
+#define CS181_FINAL_PROJECT_CPP_QLEARNING_AGENT_H
 
 #include "../include/agent.h"
 #include "../include/data.h"
@@ -10,20 +10,16 @@
 
 using Key = std::pair<Board, Action>;
 
-class Counter
-{
+class Counter {
 public: // functions
-    float operator[](const Key &idx)
-    {
-        if (auto it = dict.find(idx); it != dict.end())
-        {
+    float operator[](const Key &idx) {
+        if (auto it = dict.find(idx); it != dict.end()) {
             return it->second;
         }
         return 0.0f;
     }
 
-    void set(const Key &idx, float value)
-    {
+    void set(const Key &idx, float value) {
         dict[idx] = value;
     }
 
@@ -31,10 +27,10 @@ public: // variables
     std::map<Key, float> dict;
 };
 
-class QlearningAgent : public Agent, public EvaluationMatrix
-{
+class QlearningAgent : public Agent, public EvaluationMatrix {
 public:
-    QlearningAgent(Player player, float alpha, float gamma, float epsilon, Counter qvalue) : Agent(player), EvaluationMatrix(), myepsilon(epsilon), myalpha(alpha), mydiscount(gamma), q_value(std::move(qvalue)), myreward(0) {}
+    QlearningAgent(Player player, float alpha, float gamma, float epsilon, Counter qvalue) : Agent(player), EvaluationMatrix(), myepsilon(epsilon), myalpha(alpha), mydiscount(gamma),
+                                                                                             q_value(std::move(qvalue)), myreward(0) {}
 
     Action step() override;
 
@@ -53,13 +49,13 @@ private:
 
     float getQValue(const Board &current_board, Action action);
 
-    Action computeActionFromQValues(Board current_board);
+    Action computeActionFromQValues(const Board &current_board);
 
     float getReward(GameState state, Action action, Player player);
 
-    bool flipcoin();
+    bool flipcoin() const;
 
-    Action getAction(Board board);
+    Action getAction(const Board &board);
 };
 
 #endif
